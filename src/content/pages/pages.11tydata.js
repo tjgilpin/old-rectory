39008c340bc83c75255f63d49998df2b677a1079
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2025-03-23 22:00:52 +0000

    Move slug logic to 11ty, add nav show and ordering and slug title override

--- src/content/pages/pages.11tydata.js
+++ src/content/pages/pages.11tydata.js
@@ -7,7 +7,7 @@ export default function() {
     eleventyComputed: {
       eleventyNavigation: data => {
         // Check if the page has `eleventyNavigation.hide` set to `true`
-        if (data.eleventyNavigation && data.eleventyNavigation.hide) {
+        if (data.hide) {
           return undefined; // Exclude from navigation
         }
         // Include in navigation if no `hide` flag is set
