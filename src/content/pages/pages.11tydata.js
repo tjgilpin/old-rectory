24624d9ba188769eb0111417334daada3cd7ae44
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2025-08-27 20:18:20 +0100

    Custom slug in CMS

--- src/content/pages/pages.11tydata.js
+++ src/content/pages/pages.11tydata.js
@@ -1,14 +1,13 @@
 export default function() {
   return {
     permalink: function (data) {
-      let slug = data.title
-      slug.replace("and-", "")
+      let slug = data.slug ?? data.title;
       if (slug === 'Home') {
         return '/index.html'; // Home page
       }
       if (data.parent_page) {
         const slugPage = this.slugify(data.parent_page);
-        const slugSub = this.slugify(data.title).replace("and-", "");
+        const slugSub = this.slugify(slug);
         slug = `${slugPage}/${slugSub}`;
       } else {
         slug = this.slugify(slug);
