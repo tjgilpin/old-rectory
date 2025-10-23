82bbf8368d8d87556cd1de99f57bef13c108fa4f
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2025-10-23 22:20:45 +0100

    Use bracket notation for data files, hardcode nav order in index markdown

--- src/content/index/index.11tydata.js
+++ src/content/index/index.11tydata.js
@@ -3,37 +3,37 @@ export default {
     title: data => {
       if (data.page.fileSlug === "home" && data.home) return data.home.title;
       if (data.page.fileSlug === "rooms" && data.rooms) return data.rooms.title;
-      if (data.page.fileSlug === "food-drink" && data.fooddrink) return data.fooddrink.title;
+      if (data.page.fileSlug === "food-drink" && data['food-drink']) return data['food-drink'].title;
       if (data.page.fileSlug === "weddings" && data.weddings) return data.weddings.title;
-      if (data.page.fileSlug === "treatment-rooms" && data.treatmentrooms) return data.treatmentrooms.title;
-      if (data.page.fileSlug === "upcoming-events" && data.upcomingevents) return data.upcomingevents.title;
+      if (data.page.fileSlug === "treatment-rooms" && data['treatment-rooms']) return data['treatment-rooms'].title;
+      if (data.page.fileSlug === "upcoming-events" && data['upcoming-events']) return data['upcoming-events'].title;
       return "";
     },
     sections: data => {
       if (data.page.fileSlug === "home" && data.home) return data.home.sections;
       if (data.page.fileSlug === "rooms" && data.rooms) return data.rooms.sections;
-      if (data.page.fileSlug === "food-drink" && data.fooddrink) return data.fooddrink.sections;
+      if (data.page.fileSlug === "food-drink" && data['food-drink']) return data['food-drink'].sections;
       if (data.page.fileSlug === "weddings" && data.weddings) return data.weddings.sections;
-      if (data.page.fileSlug === "treatment-rooms" && data.treatmentrooms) return data.treatmentrooms.sections;
-      if (data.page.fileSlug === "upcoming-events" && data.upcomingevents) return data.upcomingevents.sections;
+      if (data.page.fileSlug === "treatment-rooms" && data['treatment-rooms']) return data['treatment-rooms'].sections;
+      if (data.page.fileSlug === "upcoming-events" && data['upcoming-events']) return data['upcoming-events'].sections;
       return "";
     },
     headerimage: data => {
       if (data.page.fileSlug === "home" && data.home) return data.home.headerimage;
       if (data.page.fileSlug === "rooms" && data.rooms) return data.rooms.headerimage;
-      if (data.page.fileSlug === "food-drink" && data.fooddrink) return data.fooddrink.headerimage;
+      if (data.page.fileSlug === "food-drink" && data['food-drink']) return data['food-drink'].headerimage;
       if (data.page.fileSlug === "weddings" && data.weddings) return data.weddings.headerimage;
-      if (data.page.fileSlug === "treatment-rooms" && data.treatmentrooms) return data.treatmentrooms.headerimage;
-      if (data.page.fileSlug === "upcoming-events" && data.upcomingevents) return data.upcomingevents.headerimage;
+      if (data.page.fileSlug === "treatment-rooms" && data['treatment-rooms']) return data['treatment-rooms'].headerimage;
+      if (data.page.fileSlug === "upcoming-events" && data['upcoming-events']) return data['upcoming-events'].headerimage;
       return "";
     },
     body: data => {
       if (data.page.fileSlug === "home" && data.home) return data.home.body;
       if (data.page.fileSlug === "rooms" && data.rooms) return data.rooms.body;
-      if (data.page.fileSlug === "food-drink" && data.fooddrink) return data.fooddrink.body;
+      if (data.page.fileSlug === "food-drink" && data['food-drink']) return data['food-drink'].body;
       if (data.page.fileSlug === "weddings" && data.weddings) return data.weddings.body;
-      if (data.page.fileSlug === "treatment-rooms" && data.treatmentrooms) return data.treatmentrooms.body;
-      if (data.page.fileSlug === "upcoming-events" && data.upcomingevents) return data.upcomingevents.body;
+      if (data.page.fileSlug === "treatment-rooms" && data['treatment-rooms']) return data['treatment-rooms'].body;
+      if (data.page.fileSlug === "upcoming-events" && data['upcoming-events']) return data['upcoming-events'].body;
       return "";
     },    
   }
