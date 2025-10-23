d90f5290b639cdc5f6820b4f4d53c17493200950
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2025-10-23 13:27:08 +0100

    Index pages section data

--- src/content/index/index.11tydata.js
+++ src/content/index/index.11tydata.js
@@ -1,12 +1,21 @@
 export default {
   eleventyComputed: {
-    indexTitle: data => {
+    title: data => {
       if (data.page.fileSlug === "home" && data.home) return data.home.title;
       if (data.page.fileSlug === "rooms" && data.rooms) return data.rooms.title;
       if (data.page.fileSlug === "food-drink" && data.fooddrink) return data.fooddrink.title;
       if (data.page.fileSlug === "weddings" && data.weddings) return data.weddings.title;
       if (data.page.fileSlug === "treatment-rooms" && data.treatmentrooms) return data.treatmentrooms.title;
       if (data.page.fileSlug === "upcoming-events" && data.upcomingevents) return data.upcomingevents.title;
+      return "";
+    },
+    sections: data => {
+      if (data.page.fileSlug === "home" && data.home) return data.home.sections;
+      if (data.page.fileSlug === "rooms" && data.rooms) return data.rooms.sections;
+      if (data.page.fileSlug === "food-drink" && data.fooddrink) return data.fooddrink.sections;
+      if (data.page.fileSlug === "weddings" && data.weddings) return data.weddings.sections;
+      if (data.page.fileSlug === "treatment-rooms" && data.treatmentrooms) return data.treatmentrooms.sections;
+      if (data.page.fileSlug === "upcoming-events" && data.upcomingevents) return data.upcomingevents.sections;
       return "";
     }
   }
