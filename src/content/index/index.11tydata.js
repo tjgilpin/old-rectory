ced369ff1f4f6e5bda0900d1f433374463431356
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2025-10-23 13:01:18 +0100

    Index pages

--- src/content/index/index.11tydata.js
+++ src/content/index/index.11tydata.js
@@ -2,8 +2,11 @@ export default {
   eleventyComputed: {
     displayTitle: data => {
       if (data.page.fileSlug === "home" && data.home) return data.home.title;
+      if (data.page.fileSlug === "rooms" && data.rooms) return data.rooms.title;
+      if (data.page.fileSlug === "food-drink" && data.fooddrink) return data.fooddrink.title;
       if (data.page.fileSlug === "weddings" && data.weddings) return data.weddings.title;
-      // Add more cases as needed
+      if (data.page.fileSlug === "treatment-rooms" && data.treatmentrooms) return data.treatmentrooms.title;
+      if (data.page.fileSlug === "upcoming-events" && data.upcomingevents) return data.upcomingevents.title;
       return "";
     }
   }
